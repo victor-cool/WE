0f68a7c56758f67792549e12c78109da96fd9d52
guofei <guofei@zto.com>
2019-05-14 14:19:23 +0800

    feat:init base project

---
+++ src/main/java/cool/victory/wfe/WfeApplication.java
@@ -0,0 +1,13 @@
+package cool.victory.wfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WfeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WfeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cool/victory/wfe/WfeApplicationTests.java
@@ -0,0 +1,16 @@
+package cool.victory.wfe;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WfeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
